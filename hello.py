c92991b1e757cdd6e6bd93f87e85c9e5317c43b6
Lucas Teles <110361272+LucasEduardo08@users.noreply.github.com>
2024-03-24 15:50:53 -0400

    Update gupy.py

--- hello.py
+++ hello.py
@@ -1,3 +1,33 @@
-print("Hello World!")
-print("Eu sou Lucas Eduardo.")
-print("Sou estudante de engenharia da computação na UEA")
+from math import sqrt
+
+# Questão 1
+indice = 13
+soma = k = 0
+
+while k < indice:
+    k += 1
+    soma += k
+
+print(f'Questão 1: O valor da soma é {soma}')
+
+# Questão 2
+valor = int(input("Insira um valor: "))
+
+if int(sqrt(valor))**2 == valor:
+    aux_1 = 5 * valor**2 + 4
+    aux_2 = 5 * valor**2 - 4
+
+    if int(sqrt(aux_1))**2 == (aux_1) or int(sqrt(aux_2))**2 == (aux_2):
+        print("Pertence a série de fibonacci")
+
+    else:
+        print("Não pertence a série de fibonacci") 
+        
+
+else:
+    print("Não pertence a série de fibonacci")
+
+# Questão 5
+string = input("Insira uma string:")
+
+print(string[::-1])
